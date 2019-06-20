1bdf89346b8d4ba9ee7e7be89f66651cfb2a4545
kathleen kalugdan <kay16_alcantara@yahoo.com.ph>
2019-06-20 16:25:23 +0000

    submission button

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,5 +10,16 @@ def new
   @book = Book.new
 end
 
+def create
+  Book.create(book_params)
+  redirect_to root_path
+end
+
+
+private
+
+def book_params
+  params.require(:book).permit(:title, :author, :description)
+end
 
 end
